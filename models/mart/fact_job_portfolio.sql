a1b5f2e9fe01adca5ff9187fe701fdd75ff4c397
vinaygautam1505-source <vinaygautam1505@gmail.com>
2026-03-17 12:51:53 +0530

    Update fact_job_portfolio.sql

--- models/mart/fact_job_portfolio.sql
+++ models/mart/fact_job_portfolio.sql
@@ -1,6 +1,16 @@
 SELECT
-JOB_TITLE_ID,
-COMPANY_ID,
-COMPANY_LOCATION_ID
-JOB_RATING,
-FROM {{ ref('stg_job_portfolio') }}
+j.JOB_TITLE_ID,
+c.COMPANY_ID,
+l.COMPANY_LOCATION_ID,
+j.SALARY_MIN,
+j.SALARY_MAX,
+s.JOB_RATING
+FROM {{ ref('stg_job_portfolio') }} s
+JOIN  {{ ref('dim_job_details') }} j
+ON s.JOB_TITLE_ID = j.JOB_TITLE_ID
+
+JOIN {{ ref('dim_company_profile') }} c
+ON s.COMPANY_NAME = c.COMPANY_NAME
+
+JOIN {{ ref('dim_location') }} l
+ON s.COMPANY_LOCATION = l.COMPANY_LOCATION
